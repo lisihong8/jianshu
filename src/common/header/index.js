fa9537fadc68b4fe6e8281ae06e28983e7904055
huanggengzhong <915835528@qq.com>
2020-03-23 12:33:03 +0800

    当一个组件里只有一个rend函数时 可以把这个组件写成 一个无状态组件

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {CSSTransition} from 'react-transition-group';
 import { connect } from 'react-redux';
 import {
@@ -12,48 +12,47 @@ import {
   Button
 } from './style';
 
-class Header extends Component {
+const Header = (props)=> {
+  return (
+    <HeaderWrapper>
+      {/* <Logo href="/"></Logo> */}
+      <Logo></Logo>
+      <Nav>
+        <NavItem className="left active">首页</NavItem>
+        <NavItem className="left">下载App</NavItem>
+        <NavItem className="right">登录</NavItem>
+        <NavItem className="right">
+          <i className="icon iconfont">&#xe636;</i> 
+        </NavItem>
 
-  render() {
-    return (
-      <HeaderWrapper>
-        {/* <Logo href="/"></Logo> */}
-        <Logo></Logo>
-        <Nav>
-          <NavItem className="left active">首页</NavItem>
-          <NavItem className="left">下载App</NavItem>
-          <NavItem className="right">登录</NavItem>
-          <NavItem className="right">
-            <i className="icon iconfont">&#xe636;</i> 
-          </NavItem>
+       <SearchWrapper>
+         <CSSTransition
+          timeout={500}
+          in={props.focused}
+          classNames="slide"
+         >
+          <NavSearch 
+            className={props.focused ? 'focused':''}
+            onFocus={props.handleInputFocus}
+            onBlur={props.handleInputBlur}
+            value={props.value}
+          >
+          </NavSearch>
+        </CSSTransition>
+        <i className={props.focused ? 'focused icon iconfont':'icon iconfont'}>&#xe62b;</i> 
+       </SearchWrapper>
 
-         <SearchWrapper>
-           <CSSTransition
-            timeout={500}
-            in={this.props.focused}
-            classNames="slide"
-           >
-            <NavSearch 
-              className={this.props.focused ? 'focused':''}
-              onFocus={this.props.handleInputFocus}
-              onBlur={this.props.handleInputBlur}
-              value={this.props.value}
-            >
-            </NavSearch>
-          </CSSTransition>
-          <i className={this.props.focused ? 'focused icon iconfont':'icon iconfont'}>&#xe62b;</i> 
-         </SearchWrapper>
+      </Nav>
 
-        </Nav>
-
-        <Addition>
-          <Button className="writting">  <i className="icon iconfont">&#xe608;</i> 写文章</Button>
-          <Button className="reg">注册</Button>
-        </Addition>
-      </HeaderWrapper>
-    )
-  }
+      <Addition>
+        <Button className="writting">  <i className="icon iconfont">&#xe608;</i> 写文章</Button>
+        <Button className="reg">注册</Button>
+      </Addition>
+    </HeaderWrapper>
+  )
 }
+
+
 
 const mapStateToProps = (state)=> {
   return {
