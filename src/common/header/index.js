bb4c0878ef3cdad2e378c21ffe182a32d12c3da1
huanggengzhong <915835528@qq.com>
2020-03-23 17:44:32 +0800

    使用redux-immutable统一数据格式

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -58,8 +58,13 @@ const Header = (props)=> {
 const mapStateToProps = (state)=> {
   return {
     // focused: state.header.focused
-    // state里的数据是immutable对象后,如果你要使用state里的数据,你应该要调用immutable对象里的get方法去获取state里的数据
-    focused: state.header.get('focused')
+    // state里的header数据是immutable对象后,如果你要使用state里的header里的数据,你应该要调用immutable对象里的get方法去获取state里的header数据
+    // focused: state.header.get('focused') 数据获取格式不统一,这样不太靠谱.state.header 这是按照js对象获取,header.get 这是按照immutable对象获取数据,所以说state.header.get('focused') 数据获取格式不统一,这样不太靠谱,这里的state是大的reducer里整合各个小的reducer里的state数据,希望大的reducer里的state也是immutable对象,应怎么办呢,这时需要安装依赖第三方模块redux-immutable.
+    // focused: state.header.get('focused')
+
+    // focused: state.get('header').get('focused') 等价于 focused: state.getIn(['header','focused'])
+    focused: state.getIn(['header','focused'])
+    
   }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,4 +1,5 @@
-import { combineReducers } from 'redux';
+// import { combineReducers } from 'redux';
+import { combineReducers } from 'redux-immutable';
 
 
 import { reducer as HeaderReducer } from '../common/header/store';
@@ -8,3 +9,5 @@ const reducer = combineReducers({
 });
 
 export default reducer;
+
+// 之前从redux引入的combineReducers方法 把各个小reducer整合成大的reducer 里的数据 是 普通的js对象,而从redux-immutable引入combineReducers方法 把各个小reducer整合成大的reducer 里的数据 是 immutable对象
