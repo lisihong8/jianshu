ddb4955ebbaf25a2e300888f458741a6f0c3da4b
huanggengzhong <915835528@qq.com>
2020-03-23 20:07:31 +0800

    把Header组件由原来是无状态组件 现改为 普通组件

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {CSSTransition} from 'react-transition-group';
 import { connect } from 'react-redux';
 import {actionCreators} from './store';
@@ -18,76 +18,80 @@ import {
   Button
 } from './style';
 
-const getListArea = (show)=> {
-  if(show) {
+class Header extends Component {
+
+  getListArea = (show)=> {
+    if(show) {
+      return (
+        <SearchInfo>
+            <SearchInfoTitle>
+              热门搜索
+              <SearchInfoSwitch>换一批</SearchInfoSwitch>
+            </SearchInfoTitle>
+            <SearchInfoList>
+              <SearchInfoItem>教育</SearchInfoItem>
+              <SearchInfoItem>教育</SearchInfoItem>
+              <SearchInfoItem>教育</SearchInfoItem>
+              <SearchInfoItem>教育</SearchInfoItem>
+              <SearchInfoItem>教育</SearchInfoItem>
+              <SearchInfoItem>教育</SearchInfoItem>
+              <SearchInfoItem>教育</SearchInfoItem>
+              <SearchInfoItem>教育</SearchInfoItem>
+              <SearchInfoItem>教育</SearchInfoItem>
+              <SearchInfoItem>教育</SearchInfoItem>
+              <SearchInfoItem>教育</SearchInfoItem>
+            </SearchInfoList>
+          </SearchInfo>
+      )
+    }else {
+      return null
+    }
+  }
+
+  render() {
     return (
-      <SearchInfo>
-          <SearchInfoTitle>
-            热门搜索
-            <SearchInfoSwitch>换一批</SearchInfoSwitch>
-          </SearchInfoTitle>
-          <SearchInfoList>
-            <SearchInfoItem>教育</SearchInfoItem>
-            <SearchInfoItem>教育</SearchInfoItem>
-            <SearchInfoItem>教育</SearchInfoItem>
-            <SearchInfoItem>教育</SearchInfoItem>
-            <SearchInfoItem>教育</SearchInfoItem>
-            <SearchInfoItem>教育</SearchInfoItem>
-            <SearchInfoItem>教育</SearchInfoItem>
-            <SearchInfoItem>教育</SearchInfoItem>
-            <SearchInfoItem>教育</SearchInfoItem>
-            <SearchInfoItem>教育</SearchInfoItem>
-            <SearchInfoItem>教育</SearchInfoItem>
-          </SearchInfoList>
-        </SearchInfo>
+      <HeaderWrapper>
+        {/* <Logo href="/"></Logo> */}
+        <Logo></Logo>
+        <Nav>
+          <NavItem className="left active">首页</NavItem>
+          <NavItem className="left">下载App</NavItem>
+          <NavItem className="right">登录</NavItem>
+          <NavItem className="right">
+            <i className="icon iconfont">&#xe636;</i> 
+          </NavItem>
+  
+         <SearchWrapper>
+           <CSSTransition
+            timeout={500}
+            in={this.props.focused}
+            classNames="slide"
+           >
+            <NavSearch 
+              className={this.props.focused ? 'focused':''}
+              onFocus={this.props.handleInputFocus}
+              onBlur={this.props.handleInputBlur}
+              value={this.props.value}
+            >
+            </NavSearch>
+          </CSSTransition>
+          <i className={this.props.focused ? 'focused icon iconfont':'icon iconfont'}>&#xe62b;</i> 
+  
+          {this.getListArea(this.props.focused)}
+  
+         </SearchWrapper>
+  
+        </Nav>
+  
+        <Addition>
+          <Button className="writting">  <i className="icon iconfont">&#xe608;</i> 写文章</Button>
+          <Button className="reg">注册</Button>
+        </Addition>
+      </HeaderWrapper>
     )
-  }else {
-    return null
   }
 }
 
-const Header = (props)=> {
-  return (
-    <HeaderWrapper>
-      {/* <Logo href="/"></Logo> */}
-      <Logo></Logo>
-      <Nav>
-        <NavItem className="left active">首页</NavItem>
-        <NavItem className="left">下载App</NavItem>
-        <NavItem className="right">登录</NavItem>
-        <NavItem className="right">
-          <i className="icon iconfont">&#xe636;</i> 
-        </NavItem>
-
-       <SearchWrapper>
-         <CSSTransition
-          timeout={500}
-          in={props.focused}
-          classNames="slide"
-         >
-          <NavSearch 
-            className={props.focused ? 'focused':''}
-            onFocus={props.handleInputFocus}
-            onBlur={props.handleInputBlur}
-            value={props.value}
-          >
-          </NavSearch>
-        </CSSTransition>
-        <i className={props.focused ? 'focused icon iconfont':'icon iconfont'}>&#xe62b;</i> 
-
-        {getListArea(props.focused)}
-        
-       </SearchWrapper>
-
-      </Nav>
-
-      <Addition>
-        <Button className="writting">  <i className="icon iconfont">&#xe608;</i> 写文章</Button>
-        <Button className="reg">注册</Button>
-      </Addition>
-    </HeaderWrapper>
-  )
-}
 
 
 
