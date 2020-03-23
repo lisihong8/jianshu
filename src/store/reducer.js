db8e7036ed29e573241059309360dc996955881d
huanggengzhong <915835528@qq.com>
2020-03-23 14:18:17 +0800

    优化代码1

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-import HeaderReducer from '../common/header/store/reducer';
+import { reducer as HeaderReducer } from '../common/header/store';
 
 const reducer = combineReducers({
   header: HeaderReducer
