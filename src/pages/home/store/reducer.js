7134baec2c870ee397733642c28cd77fecec2cad
huanggengzhong <915835528@qq.com>
2020-03-25 18:39:33 +0800

    优化home文件里的reducer.js代码

--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -8,23 +8,31 @@ const defaultState = fromJS({
   recommendList:[],
   articlePage: 1,
   showScroll: false
-})
+});
+
+const changeHomeData = (state,action)=> {
+  return state.merge({
+    topicList: fromJS(action.topicList),
+    articleList: fromJS(action.articleList),
+    recommendList: fromJS(action.recommendList)
+  });
+};
+
+const addArticleList = (state,action)=> {
+  // return state.set('articleList',state.get('articleList').concat(action.list)).set('articlePage',action.nextPage);
+  return state.merge({
+    articleList: state.get('articleList').concat(action.list),
+    articlePage: action.nextPage
+  });
+}
 
 export default (state=defaultState,action)=> {
   switch(action.type) {
     case contants.CHANGE_HOME_DATA:
       // console.log(action);
-      return state.merge({
-          topicList: fromJS(action.topicList),
-          articleList: fromJS(action.articleList),
-          recommendList: fromJS(action.recommendList)
-        });
+      return changeHomeData(state,action);
     case contants.GET_LOAD_MORE:
-      // return state.set('articleList',state.get('articleList').concat(action.list)).set('articlePage',action.nextPage);
-      return state.merge({
-        articleList: state.get('articleList').concat(action.list),
-        articlePage: action.nextPage
-      });
+      return addArticleList(state,action);
     case contants.TOGGLE_TOP_SHOW:
       return state.set('showScroll',action.show);
     default:
